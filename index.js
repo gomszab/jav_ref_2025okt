1c4af6833d37f9712e9d0253b2fd466808cc539a
gomszab <gombosszabolcs88@gmail.com>
2025-10-31 13:53:52 +0100

    objektum bevezetese

--- index.js
+++ index.js
@@ -3,41 +3,41 @@
  */
 const tabletitle = 'Javítási referencia'; // tabletitle valtozo deklaralasa aminek egy string tipusu erteket
 /**
- * @type {string} valtozo ami tartalmazza a keresztnevet, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ * @type {{title: string}} valtozo ami tartalmazza tablazat header elso oszlopanak tulajdonsagait
  */
-const tableheaderfirstname = 'Keresztnév'; // tableheaderfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+const tableheaderfirstname = { // tableheaderformname valtozo deklaralasa, aminek egy objektum tipusu erteket adunk
+    title:'Keresztnév' // az objektum title tulajdonsaganak egy string tipusu erteket adunk
+}; 
 /**
- * @type {string} valtozo ami tartalmazza a vezeteknevet, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ * @type {{title: string}} valtozo ami tartalmazza tablazat header második oszlopanak tulajdonsagait
  */
-const tableheaderlastname = 'Vezetéknév'; // tableheaderlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+const tableheaderlastname = { // tableheaderlastname valtozo deklaralasa, aminek egy objektum tipusu erteket adunk
+    title:'Vezetéknév' // az objektum title tulajdonsaganak egy string tipusu erteket adunk
+}; 
 /**
- * @type {string} valtozo ami tartalmazza a Dienes szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
- */
-const tablefirstrowlastname = 'Dienes'; // tablefirstrowlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+ * @type {{ lastName: string, middleName: string, firstName: string}} valtozo ami osszevonva tartalmazza az elso sor ertekeit 
+ */
+const firstRowObject = {  // firstRowObject valtozo deklaralasa, ami egy objektum tipusu ertekkel lesz egyenlo
+    lastName: 'Dienes', // az objektum lastName tulajdonsaganak egy string tipusu erteket adunk
+    middleName: 'Zoltán', // az objektum middleName tulajdonsaganak egy string tipusu erteket adunk
+    firstName: 'Pál' // az objektum firstName tulajdonsaganak egy string tipusu erteket adunk
+};
+
 /**
- * @type {string} valtozo ami tartalmazza a Zoltán szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
- */
-const tablefirstrowmiddlename = 'Zoltán'; // tablefirstrowmidlename valtozo deklaralasa, aminek egy string tipusu erteket adunk
+ * @type {{ lastName: string, firstName: string}} valtozo ami osszevonva tartalmazza a masodik sor ertekeit 
+ */
+const secondRowObject = {  // secondRowObject valtozo deklaralasa, ami egy objektum tipusu ertekkel lesz egyenlo
+    lastName: 'Obádovics', // az objektum lastName tulajdonsaganak egy string tipusu erteket adunk
+    firstName: 'Gyula' // az objektum firstName tulajdonsaganak egy string tipusu erteket adunk
+};
+
 /**
- * @type {string} valtozo ami tartalmazza a Pál szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
- */
-const tablefirstrowfirstname = 'Pál'; // tablefirstrowfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
-/**
- * @type {string} valtozo ami tartalmazza a Obádovics szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
- */
-const tablesecondrowlastname = 'Obádovics'; // tablesecondrowlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
-/**
- * @type {string} valtozo ami tartalmazza a Gyula szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
- */
-const tablesecondrowfirstname = 'Gyula'; // tablesecondrowfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
-/**
- * @type {string} valtozo ami tartalmazza a Dávid szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
- */
-const tablethirdrowlastname = 'Dávid'; // tablethirdrowlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
-/**
- * @type {string} valtozo ami tartalmazza a Gyula szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
- */
-const tablethirdrowfirstname = 'Gyula'; // tablethirdrowfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+ * @type {{ lastName: string, firstName: string}} valtozo ami osszevonva tartalmazza a harmadik sor ertekeit 
+ */
+const thirdRowObject = {  // thirdRowObject valtozo deklaralasa, ami egy objektum tipusu ertekkel lesz egyenlo
+    lastName: 'Dávid', // az objektum lastName tulajdonsaganak egy string tipusu erteket adunk
+    firstName: 'Gyula' // az objektum firstName tulajdonsaganak egy string tipusu erteket adunk
+};
 
 /**
  * @type {HTMLDivElement} ami tartalmaz egy div elemet a weboldal cimenek
@@ -56,14 +56,14 @@ document.body.appendChild(divforheader); // hozzafuzom a letrehozott elemet a ht
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazatfejlec elso oszlopa
  */
 const firstnameheader = document.createElement('span'); // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy firstnameheader nevu valtozoba
-firstnameheader.innerText = tableheaderfirstname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tableheaderforname valtozo erteket ami egy string es erteke Keresztnév
+firstnameheader.innerText = tableheaderfirstname.title; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tableheaderforname változó title tulajdonságának erteket ami egy string es erteke Keresztnév
 divforheader.appendChild(firstnameheader) /// hozzafuzzuk a korabban letrehozott divforheader divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami  a tablazatfejlec masodik oszlopa
  */
 const lastnameheader = document.createElement('span'); // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy firstnameheader nevu valtozoba
-lastnameheader.innerText = tableheaderlastname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tableheaderlastname valtozo erteket ami egy string es erteke Vezetéknév
+lastnameheader.innerText = tableheaderlastname.title; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tableheaderlastname változó title tulajdonságának erteket ami egy string es erteke Vezetéknév
 divforheader.appendChild(lastnameheader) // hozzafuzzuk a korabban letrehozott divforheader divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
@@ -76,21 +76,21 @@ document.body.append(htmlfirstrowdiv);  // hozzafuzom a letrehozott elemet a htm
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazat elso sora és elso oszlopa
  */
 const htmlFirstRowVeznev = document.createElement('span');  // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy htmlFirstRowVeznev nevu valtozoba
-htmlFirstRowVeznev.innerText = tablefirstrowlastname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tablefirstrowlastname valtozo erteket ami egy string es erteke Dienes
+htmlFirstRowVeznev.innerText = firstRowObject.lastName; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a firstRowObject lastname tulajdonságának erteket ami egy string es erteke Dienes
 htmlfirstrowdiv.appendChild(htmlFirstRowVeznev); // hozzafuzzuk a korabban letrehozott htmlfirstrowdiv divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazat elso sora masodik oszlopa 
  */
 const htmlFirstRowKoznev = document.createElement('span'); // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy htmlFirstRowKoznev nevu valtozoba
-htmlFirstRowKoznev.innerText = tablefirstrowmiddlename; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tablefirstrowmiddlename valtozo erteket ami egy string es erteke Zoltán
+htmlFirstRowKoznev.innerText = firstRowObject.middleName; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a firstRowObject middlename tulajdonságának erteket ami egy string es erteke Zoltán
 htmlfirstrowdiv.appendChild(htmlFirstRowKoznev); // hozzafuzzuk a korabban letrehozott htmlfirstrowdiv divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazat elso sora harmadik oszlopa 
  */
 const htmlFirstRowKernev = document.createElement('span'); // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy htmlFirstRowKernev nevu valtozoba
-htmlFirstRowKernev.innerText = tablefirstrowfirstname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tablefirstrowfirstname valtozo erteket ami egy string es erteke Pál
+htmlFirstRowKernev.innerText = firstRowObject.firstName; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a firstRowObject firstname tulajdonságának erteket ami egy string es erteke Pál
 htmlfirstrowdiv.appendChild(htmlFirstRowKernev); // hozzafuzzuk a korabban letrehozott htmlfirstrowdiv divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
@@ -103,14 +103,14 @@ document.body.append(htmlsecondrowdiv);  // hozzafuzom a letrehozott elemet a ht
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazat masodik sora elso oszlopa
  */
 const htmlSecondRowVeznev = document.createElement('span');  // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy htmlSecondRowVeznev nevu valtozoba
-htmlSecondRowVeznev.innerText = tablesecondrowlastname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tablesecondrowlastname valtozo erteket ami egy string es erteke Obádovics
+htmlSecondRowVeznev.innerText = secondRowObject.lastName; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a secondRowObject lastName tulajdonságának erteket ami egy string es erteke Obádovics
 htmlsecondrowdiv.appendChild(htmlSecondRowVeznev); // hozzafuzzuk a korabban letrehozott htmlsecondrowdiv divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazat masodik sora masodik oszlopa 
  */
 const htmlSecondRowKernev = document.createElement('span'); // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy htmlSecondRowKernev nevu valtozoba
-htmlSecondRowKernev.innerText = tablesecondrowfirstname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tablesecondrowfirstname valtozo erteket ami egy string es erteke Gyula
+htmlSecondRowKernev.innerText = secondRowObject.firstName; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a secondRowObject firstname tulajdonságának erteket ami egy string es erteke Gyula
 htmlsecondrowdiv.appendChild(htmlSecondRowKernev); // hozzafuzzuk a korabban letrehozott htmlsecondrowdiv divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
@@ -123,12 +123,12 @@ document.body.append(htmlthirdrowdiv);  // hozzafuzom a letrehozott elemet a htm
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazat harmadik sora elso oszlopa
  */
 const htmlThirdRowVeznev = document.createElement('span');  // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy htmlThirdRowVeznev nevu valtozoba
-htmlThirdRowVeznev.innerText = tablethirdrowlastname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tablethirdrowlastname valtozo erteket ami egy string es erteke Dávid
+htmlThirdRowVeznev.innerText = thirdRowObject.lastName; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a thirdRowObject lastName tulajdonságának erteket ami egy string es erteke Dávid
 htmlthirdrowdiv.appendChild(htmlThirdRowVeznev); // hozzafuzzuk a korabban letrehozott htmlthirdrowdiv divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
 
 /**
  * @type {HTMLSpanElement} ami tartalmaz egy span elemet, ami a tablazat harmadik sora masodik oszlopa
  */
 const htmlThirdRowKernev = document.createElement('span'); // meghivom a document.createElementet egy stringel, es beleteszem a visszateresi erteket egy htmlThirdRowKernev nevu valtozoba
-htmlThirdRowKernev.innerText = tablethirdrowfirstname; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a tablethirdrowfirstname valtozo erteket ami egy string es erteke Gyula
+htmlThirdRowKernev.innerText = thirdRowObject.firstName; // beallitjuk a span element tartalmanak az innerText tulajdonsagon keresztul a thirdRowObject firstname tulajdonságának erteket ami egy string es erteke Gyula
 htmlthirdrowdiv.appendChild(htmlThirdRowKernev); // hozzafuzzuk a korabban letrehozott htmlthirdrowdiv divhez a most beallitott span elementet tartalmazo valtozonk tartalmat
