8898a2c16ec8e93e592b3661118726e87c764240
gomszab <gombosszabolcs88@gmail.com>
2025-10-31 13:26:00 +0100

    valtozok bevezetese

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-console.log("Javitasi referencia"); // kiiratom konzolra a weboldal nevet a console.log fuggveny hivasanak segitsegevel
-console.log("Keresztnév Vezetéknév" ); // kiiratom konzolra a tablazat fejlecet a console.log fuggveny hivasanak segitsegevel
-console.log("Dienes Zoltán Pál"); // kiiratom konzolra a tablazat elso sorat a console.log fuggveny hivasanak segitsegevel
-console.log("Obádovics Gyula"); // kiiratom konzolra a tablazat masodik sorat a console.log fuggveny hivasanak segitsegevel
-console.log("Dávid Gyula"); // kiiratom konzolra a harmadik sorat a console.log fuggveny hivasanak segitsegevel
+/**
+ * @type {string} valtozo ami tartalmazza a cimet, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tabletitle = 'Javítási referencia'; // tabletitle valtozo deklaralasa aminek egy string tipusu erteket
+/**
+ * @type {string} valtozo ami tartalmazza a keresztnevet, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tableheaderfirstname = 'Keresztnév'; // tableheaderfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a vezeteknevet, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tableheaderlastname = 'Vezetéknév'; // tableheaderlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a Dienes szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tablefirstrowlastname = 'Dienes'; // tablefirstrowlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a Zoltán szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tablefirstrowmiddlename = 'Zoltán'; // tablefirstrowmidlename valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a Pál szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tablefirstrowfirstname = 'Pál'; // tablefirstrowfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a Obádovics szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tablesecondrowlastname = 'Obádovics'; // tablesecondrowlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a Gyula szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tablesecondrowfirstname = 'Gyula'; // tablesecondrowfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a Dávid szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tablethirdrowlastname = 'Dávid'; // tablethirdrowlastname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+/**
+ * @type {string} valtozo ami tartalmazza a Gyula szoveget, mivel const ezert a kesobbiekben nem valtoztathatjuk
+ */
+const tablethirdrowfirstname = 'Gyula'; // tablethirdrowfirstname valtozo deklaralasa, aminek egy string tipusu erteket adunk
+
+console.log(tabletitle); // kiiratom konzolra a weboldal nevet a console.log fuggveny hivasanak segitsegevel
+console.log(tableheaderfirstname + " " + tableheaderlastname); // kiiratom konzolra a tablazat fejlecet a console.log fuggveny hivasanak segitsegevel
+console.log(tablefirstrowlastname + " " + tablefirstrowmiddlename + " " + tablefirstrowfirstname); // kiiratom konzolra a tablazat elso sorat a console.log fuggveny hivasanak segitsegevel
+console.log(tablesecondrowlastname + " " + tablesecondrowfirstname); // kiiratom konzolra a tablazat masodik sorat a console.log fuggveny hivasanak segitsegevel
+console.log(tablethirdrowlastname + " " + tablethirdrowfirstname); // kiiratom konzolra a harmadik sorat a console.log fuggveny hivasanak segitsegevel
