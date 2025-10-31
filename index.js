3a6411f5c1817533b7ec1ba3bb4c99a8fbcaf83b
gomszab <gombosszabolcs88@gmail.com>
2025-10-31 13:17:03 +0100

    konzolra kiirjuk a stringeket

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Javitasi referencia"); // kiiratom konzolra a weboldal nevet a console.log fuggveny hivasanak segitsegevel
+console.log("Keresztnév Vezetéknév" ); // kiiratom konzolra a tablazat fejlecet a console.log fuggveny hivasanak segitsegevel
+console.log("Dienes Zoltán Pál"); // kiiratom konzolra a tablazat elso sorat a console.log fuggveny hivasanak segitsegevel
+console.log("Obádovics Gyula"); // kiiratom konzolra a tablazat masodik sorat a console.log fuggveny hivasanak segitsegevel
+console.log("Dávid Gyula"); // kiiratom konzolra a harmadik sorat a console.log fuggveny hivasanak segitsegevel
